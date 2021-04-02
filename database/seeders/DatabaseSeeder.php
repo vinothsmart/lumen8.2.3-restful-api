f70b29e31fa884747383e47e6ec4c949c215156c
vinothsmart <s.vinothkanna90@gmail.com>
2021-04-02 12:44:44 +0530

    UserSeeder::class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
