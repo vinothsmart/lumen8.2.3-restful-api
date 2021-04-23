90b7993df8a8ab482a8ad640942b42be5ba4f7bd
vinothsmart <s.vinothkanna90@gmail.com>
2021-04-23 16:21:17 +0530

    updated routes common api prefix

--- routes/web.php
+++ routes/web.php
@@ -21,28 +21,29 @@ $router->get('/phpinfo', function () use ($router) {
     phpinfo();
 });
 
-/**
- * Roles
- */
+
 // $router->group(['prefix' => 'api/admin'], function () use ($router) {
 $router->group(['prefix' => 'api'], function () use ($router) {
+
+    /**
+     * Roles
+     */
     $router->post('roles', ['as' => 'roles.store', 'uses' => 'Role\RoleController@store']);
     $router->get('roles', ['as' => 'roles.index', 'uses' => 'Role\RoleController@index']);
     $router->get('rolesList', ['as' => 'roles.list', 'uses' => 'Role\RoleController@list']);
     $router->get('roles/{role}', ['as' => 'roles.show', 'uses' => 'Role\RoleController@show']);
     $router->put('roles/{role}', ['as' => 'roles.update', 'uses' => 'Role\RoleController@update']);
+    $router->patch('roles/{role}', ['as' => 'roles.patchupdate', 'uses' => 'Role\RoleController@update']);
     $router->delete('roles/{role}', ['as' => 'roles.delete', 'uses' => 'Role\RoleController@destroy']);
     $router->get('roles/{role}/users', ['as' => 'roles.users', 'uses' => 'Role\RoleUserController@index']);
-});
 
-
-/**
- * Users
- */
-$router->group(['prefix' => 'api'], function () use ($router) {
+    /**
+     * Users
+     */
     $router->post('users', ['as' => 'users.store', 'uses' => 'User\UserController@store']);
     $router->get('users', ['as' => 'users.index', 'uses' => 'User\UserController@index']);
     $router->get('users/{user}', ['as' => 'users.show', 'uses' => 'User\UserController@show']);
     $router->put('users/{user}', ['as' => 'users.update', 'uses' => 'User\UserController@update']);
+    $router->patch('users/{user}', ['as' => 'users.patchupdate', 'uses' => 'User\UserController@update']);
     $router->delete('users/{user}', ['as' => 'users.delete', 'uses' => 'User\UserController@destroy']);
 });
