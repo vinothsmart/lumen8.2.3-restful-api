57a45ec36500e657d511111b8e7bdcda7ad4444f
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-04 16:22:50 +0530

    roles getting hasMany relationship

--- app/Http/Controllers/Role/RoleController.php
+++ app/Http/Controllers/Role/RoleController.php
@@ -15,7 +15,8 @@ class RoleController extends ApiController
      */
     public function index()
     {
-        $roles= Role::all();
+        // $roles= Role::all();
+        $roles = Role::has('users')->get();
 
         return $this->showAll($roles);
     }
