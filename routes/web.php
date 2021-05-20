d75ea05abd3c74ccab89fa5159414f3f01fc9e36
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-20 13:48:46 +0530

    add role

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,8 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     /**
      * Roles
      */
-    $router->post('roles', ['as' => 'roles.store', 'uses' => 'Role\RoleController@store']);
     $router->get('roles', ['as' => 'roles.index', 'uses' => 'Role\RoleController@index']);
+    $router->post('roles', ['as' => 'roles.store', 'uses' => 'Role\RoleController@store']);
     $router->get('rolesList', ['as' => 'roles.list', 'uses' => 'Role\RoleController@list']);
     $router->get('roles/{role}', ['as' => 'roles.show', 'uses' => 'Role\RoleController@show']);
     $router->put('roles/{role}', ['as' => 'roles.update', 'uses' => 'Role\RoleController@update']);
@@ -40,8 +40,8 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     /**
      * Users
      */
-    $router->post('users', ['as' => 'users.store', 'uses' => 'User\UserController@store']);
     $router->get('users', ['as' => 'users.index', 'uses' => 'User\UserController@index']);
+    $router->post('users', ['as' => 'users.store', 'uses' => 'User\UserController@store']);
     $router->get('users/{user}', ['as' => 'users.show', 'uses' => 'User\UserController@show']);
     $router->put('users/{user}', ['as' => 'users.update', 'uses' => 'User\UserController@update']);
     $router->patch('users/{user}', ['as' => 'users.patchupdate', 'uses' => 'User\UserController@update']);
