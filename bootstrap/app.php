b79fb330b6b4109b8b28dd1279f7479d2db6b12c
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-01 15:00:05 +0530

    mail configuaration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -75,6 +75,15 @@ $app->configure('app');
 
 $app->configure('filesystems');
 
+$app->configure('mail');
+
+$app->alias('mail.manager', Illuminate\Mail\MailManager::class);
+$app->alias('mail.manager', Illuminate\Contracts\Mail\Factory::class);
+
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
+
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
@@ -105,11 +114,13 @@ $app->configure('filesystems');
 |
  */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+
+$app->register(Illuminate\Mail\MailServiceProvider::class);
 
 
 /*
