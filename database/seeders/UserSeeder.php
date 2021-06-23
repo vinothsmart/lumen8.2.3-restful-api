07b460cd0c84b0217ed5c656eeef83ad92d923b5
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-23 16:34:01 +0530

    truncate and flushevents updated in userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        // Truncate the user table
+        User::truncate();
+
+        // Clear the user events
+        User::flushEventListeners();
+
         User::factory()->create(['name' => 'Vino', 'email' => 'vino@ab.com', 'password' => '$2y$10$1KeHVDk4jl1LU7psksi9iet369iU7eli4fshl/zXcIxQqKCLMTZSW', 'image' => 'default.jpg', 'verified' => 1, 'admin' => User::ADMIN_USER, 'verification_token' => null]);
 
         $usersQunatity = 1000;
