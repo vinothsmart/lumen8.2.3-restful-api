fd8a6af6358d0f4637f9c19dfca4df0996b51670
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-23 17:21:25 +0530

    php artisan make:mail Test -m emails.test

--- app/Mail/Test.php
+++ app/Mail/Test.php
@@ -2,27 +2,23 @@
 
 namespace App\Mail;
 
-use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
-class UserMailChanged extends Mailable
+class Test extends Mailable
 {
     use Queueable, SerializesModels;
-
-    public $user;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($user)
+    public function __construct()
     {
-        // $userFindBYId = User::findOrFail($user);
-        $this->user = $user;
+        //
     }
 
     /**
@@ -32,6 +28,6 @@ class UserMailChanged extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.confirm')->subject('Please confirm your new email');
+        return $this->markdown('emails.test');
     }
 }
